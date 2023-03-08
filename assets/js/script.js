ceceec59521f2d31e420c65282258c9a992833c5
satogako <genevaq@ukr.net>
2023-03-08 00:15:43 +0000

    Add code to function runGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+/**
+ * Wait for the DOM to finish loading before runing the game
+ * Get the button elements and add event listeners to them
+ */
 document.addEventListener('DOMContentLoaded', function() {
 
     let buttons = document.getElementsByTagName('button');
@@ -5,14 +9,36 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let button of buttons) {
         button.addEventListener('click', function() {
             let userChoise = this.getAttribute('data-type');
-            alert(`You press batton "${userChoise}"!`);
             runGame(userChoise);
         });
     }
 
 })
 
-function runGame() {
+/**
+ * Receives a data-type from the event listener,
+ * determines the name of the image and passes it to
+ * function displayOppoPlayer.
+ * Starts the function computerChoise.
+ */
+function runGame(userChoise) {
+
+    let imageName;
+
+    if (userChoise === 'rock') {
+        imageName = 'rock.png'
+    } else if (userChoise === 'paper') {
+        imageName = 'paper.png'
+    } else if (userChoise === 'scissors') {
+        imageName = 'scissors.png'
+    } else if (userChoise === 'lizard') {
+        imageName = 'lizard.png'
+    } else if (userChoise === 'spock') {
+        imageName = 'spock.png'
+    }
+
+    displayOppoPlayer(imageName);
+    computerChoise();
 
 }
 
@@ -28,8 +54,9 @@ function calculateCorrectAnswer() {
 
 }
 
-function displayOppoPlayer() {
+function displayOppoPlayer(imageName) {
 
+    
 }
 
 function displayOppoComputer() {
